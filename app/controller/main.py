fd3db1ff8ab81b9d696d311283d1309470fd9de4
Lemmah <lemayiannakolah@gmail.com>
2019-02-24 06:48:09 +0300

    create registration view for lawyers

--- app/controller/main.py
+++ app/controller/main.py
@@ -10,13 +10,19 @@ def index():
     return render_template('main/index.html',
                            title='Boda Justice')
 
+
+@bp.route('/login')
+def login():
+    return render_template('main/index.html',
+                            title='Boda Justice')
+
 @bp.route('/lawyers')
 def lawyers_dashboard():
     return render_template('lawyers/index.html',
                             title='Boda Justice',
                             logged_in=True)
 
-@bp.route('/login')
-def login():
-    return render_template('main/index.html',
+@bp.route('/lawyers/register')
+def lawyers_registration():
+    return render_template('lawyers/registration.html',
                             title='Boda Justice')

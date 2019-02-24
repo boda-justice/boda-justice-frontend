0eec5911a07985bc07351ec878f225cae0320a50
Lemmah <lemayiannakolah@gmail.com>
2019-02-24 05:29:48 +0300

    route login view

--- app/controller/main.py
+++ app/controller/main.py
@@ -13,4 +13,10 @@ def index():
 @bp.route('/lawyers')
 def lawyers_dashboard():
     return render_template('lawyers/index.html',
+                            title='Boda Justice',
+                            logged_in=True)
+
+@bp.route('/login')
+def login():
+    return render_template('main/index.html',
                             title='Boda Justice')
